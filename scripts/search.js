9436cfd185b787f4a8d403ff32375bfd493ad347
Lucas Imbá <imbalucas@gmail.com>
2025-10-20 21:40:37 -0300

    20/10

--- scripts/search.js
+++ scripts/search.js
@@ -1,26 +1,39 @@
 function  searchVideo (event) {
-    const search = event.target.value;
-    console.log(search, event.target);
+    const search = event.target.value.toLowerCase();    
     // toda vez que digitamos, dispara um evento
-
-    const videos = document.querySelectorAll(".video-card");
-    // console.log(videos);
+    const videos = document.querySelectorAll(".video-card");   
 
     // loop de iteracao para validar se o termo é existente: nome da categoria, nome do canal e o titulo do video
 
     // Array, lista, map: se escreve o nome da variavel em plural
     // o elemento se escreve no singular
     for (const video of videos) {
-        // capturar as informações e armazenar em um objeto
+        console.log("Video:: ", video)
+        // desestruturar objeto
+        const [title] = video.getElementsByTagName("h3");
+        const [channelName, category] = video.getElementsByTagName("p");
+        // const [, category] = video.getElementsByTagName("p"); caso queria apenas o segundo
+
+        // capturar as informações e armazenar em um objeto        
         const videoInfo = {
-        // title: video.getElementsByTagName("h3"),
-        // channelName: video.getElementsByTagName("p"),
-        // category: video.getElementsByTagName("p")
-        title: video.getElementsByTagName("h3")[0].innerText,
-        channelName: video.getElementsByTagName("p")[0].innerText,
-        category: video.getElementsByTagName("p")[1].innerText
+            title: title.innerText.toLowerCase(), // title: video.getElementsByTagName("h3")[0].innerText,
+            channelName: channelName.innerText.toLowerCase(), // channelName: video.getElementsByTagName("p")[0].innerText,
+            category: category.innerText.toLowerCase() // category: video.getElementsByTagName("p")[1].innerText        
+            // innerHTML também funciona
         }
-        // innerHTML também funciona
-        console.log(videoInfo);
+
+        // reverte qualquer estilização colocada no elemento
+        video.style.display = 'revert';
+
+        if(!matches(videoInfo, search)) video.style.display ='none'
+            
     }
+}
+
+function matches (videoInfo, searchTerm) {
+    return (
+        videoInfo.title.includes(searchTerm) ||
+        videoInfo.channelName.includes(searchTerm) ||
+        videoInfo.category.includes(searchTerm)
+    );
 }
